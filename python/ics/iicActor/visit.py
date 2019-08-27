bca6b90e5291c68519db349190e9ec61a5e4f9e4
Craig Loomis <cloomis@astro.princeton.edu>
2019-08-27 03:49:46 -1000

    Start a visit manager.
    - Fetches visits from gen2
    - arranges to pass down either visits or frames to other actors.
    - tracks an "active" visit.

--- python/ics/iicActor/visit.py
+++ python/ics/iicActor/visit.py
@@ -1,22 +1,98 @@
+import threading
+
 class VisitActiveError(Exception):
     pass
 
 class VisitNotActiveError(Exception):
     pass
 
+class VisitOverflowed(object):
+    pass
+
+class VisitAlreadyDone(object):
+    pass
+
 class VisitManager(object):
+    def __init__(self, actor):
+        self.actor = actor
+        self.activeVisit = None
+
+    def __enter__(self):
+        return self.newVisit()
+    def __exit__(self):
+        self.releaseVisit()
+
     def newVisit(self, name=None):
         """ Allocate a new visit. """
 
-class Visit(object):
-    def __init__(self):
-        self._fetchFromGen2()
+        if self.activeVisit is not None:
+            raise VisitActiveError()
+        visit = self._fetchVisitFromGen2()
 
-    def _fetchFromGen2(self):
-        raise NotImplementedError()
+        self.activeVisit = Visit(visitId=visit, name=name)
+        return self.activeVisit
+
+    def releaseVisit(self):
+        if self.activeVisit is None:
+            raise VisitNotActiveError()
+
+        self.activeVisit.stop()
+        self.activeVisit = None
+
+    def _fetchVisitFromGen2(self):
+        """Actually get a new visit from Gen2.
+
+
+        What PFS calls a "visit", Gen2 calls a "frame".
+
+        """
+
+        ret = self.actor.cmdr.call(actor='gen2', cmdStr='getVisit', timeLim=10.0)
+        if ret.didFail:
+            raise RuntimeError("VisitManager failed to get a visit number in 10s!")
+
+        visit = self.actor.models['gen2'].keyVarDict['visit'].valueList[0]
+        return visit
+
+
+class Visit(object):
+    def __init__(self, visitId, name=None):
+        self.visitId = visitId
+        self.name = name
+        self.__agcFrameId = 0
+        self.__fpsFrameId = 0
+        self.iAmDead = False
+        self.idLock = threading.RLock()
+
+    def __str__(self):
+        return (f"Visit(name={self.name} visitId={self.visitId} "
+                f"fpsFrame={self.__fpsFrameId} agcFrame={self.__agcFrameId})")
+
+    def stop(self):
+        """Declare that we should not be used any more."""
+
+        self.iAmDead = True
 
     def frameForAGC(self):
-        raise NotImplementedError()
+        if self.iAmDead:
+            raise VisitAlreadyDone()
+
+        with self.idLock:
+            frameIdx = self.__agcFrameId
+            if frameIdx >= 100:
+                raise VisitOverflowed()
+            self.__agcFrameId += 1
+
+        return self.visitId*100 + frameIdx
 
     def frameForFPS(self):
-        raise NotImplementedError()
+        if self.iAmDead:
+            raise VisitAlreadyDone()
+
+        with self.idLock:
+            frameIdx = self.__fpsFrameId
+            if frameIdx >= 100:
+                raise VisitOverflowed()
+            self.__fpsFrameId += 1
+
+        return self.visitId*100 + frameIdx
