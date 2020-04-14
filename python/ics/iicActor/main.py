d3ca3635f65c1465c1ea98f1e5b3142e7c899700
Arnaud Le Fur <arnaud.lefur@lam.fr>
2020-04-14 19:14:10 +0200

    add sps model

--- python/ics/iicActor/main.py
+++ python/ics/iicActor/main.py
@@ -3,16 +3,16 @@
 import actorcore.ICC
 from ics.iicActor import visit
 
+
 class IicActor(actorcore.ICC.ICC):
     def __init__(self, name,
                  productName=None,
                  debugLevel=30):
-
         """ Setup an Actor instance. See help for actorcore.Actor for details. """
 
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        actorcore.ICC.ICC.__init__(self, name, 
+        actorcore.ICC.ICC.__init__(self, name,
                                    productName=productName)
 
         # No quite sure what to do about this yet. But one of the
@@ -25,18 +25,19 @@ class IicActor(actorcore.ICC.ICC):
 
     def connectionMade(self):
         if self.everConnected is False:
-            self.logger.info("Establishing first tron connection...")
+            self.logger.info('Establishing first tron connection...')
             self.everConnected = True
 
-            _needModels = [self.name, "gen2",
-                           "fps", "mcs"]
+            _needModels = [self.name, 'gen2', 'fps', 'mcs', 'sps']
             self.logger.info(f'adding models: {sorted(_needModels)}')
             self.addModels(_needModels)
             self.logger.info(f'added models: {sorted(self.models.keys())}')
 
+
 def main():
     theActor = IicActor('iic', productName='iicActor')
     theActor.run()
+
 
 if __name__ == '__main__':
     main()
