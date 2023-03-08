635c81d2a8f4f434eb60b15ec8074070f9317dd1
Arnaud Le Fur <afur@princeton.astro.edu>
2023-03-08 18:39:40 -0500

    split into keyRepo.py

--- python/ics/iicActor/utils/engine.py
+++ python/ics/iicActor/utils/engine.py
@@ -2,26 +2,8 @@ import ics.iicActor.utils.opdb as opdbUtils
 from ics.utils.threading import singleShot
 from ics.utils.visit import visitManager
 from iicActor.utils import registry
+from iicActor.utils import keyRepo
 from iicActor.utils import resourceManager
-
-
-class KeyRepo(object):
-    def __init__(self, engine):
-        self.engine = engine
-
-    @property
-    def actor(self):
-        return self.engine.actor
-
-    def enuKeys(self, cams, keyName):
-        """Get enu keys for that sequence given a key name."""
-        specNames = list(set([cam.specName for cam in cams]))
-        values = [self.actor.models[f'enu_{specName}'].keyVarDict[keyName].getValue() for specName in specNames]
-        return dict([(specName, value) for specName, value in zip(specNames, values)])
-
-    def hexapodPoweredOff(self, cams):
-        """Return specName where the hexapod is powered off."""
-        return [specName for specName, (_, state, _, _, _) in self.enuKeys(cams, 'pduPort3').items() if state == 'off']
 
 
 class Engine(object):
@@ -33,7 +15,7 @@ class Engine(object):
         self.resourceManager = resourceManager.ResourceManager(actor)
         self.visitManager = visitManager.VisitManager(actor)
         self.registry = registry.Registry(self)
-        self.keyRepo = KeyRepo(self)
+        self.keyRepo = keyRepo.KeyRepo(self)
 
     @singleShot
     def runInThread(self, *args, **kwargs):
