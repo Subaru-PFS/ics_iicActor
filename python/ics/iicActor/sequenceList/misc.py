2b857064b2f6d07c4bf48ebe447da63b915f4be8
Arnaud Le Fur <afur@astro.princeton.edu>
2024-05-06 18:27:33 -0400

    oversight turn on/off cableB illuminator for dotCrossing

--- python/ics/iicActor/sequenceList/misc.py
+++ python/ics/iicActor/sequenceList/misc.py
@@ -36,6 +36,7 @@ class DotCrossing(FpsSequence):
         # turning illuminators on
         self.add('sps', 'bia on')
         self.add('peb', 'led on')
+        self.add('dcb', 'power on cableB')
 
         self.add('mcs', 'expose object', parseFrameId=True, exptime=exptime, doFibreId=True)
 
@@ -46,6 +47,7 @@ class DotCrossing(FpsSequence):
         # turning illuminators off
         self.tail.add('sps', 'bia off')
         self.tail.add('peb', 'led off')
+        self.tail.add('dcb', 'power off cableB')
 
     @classmethod
     def fromCmdKeys(cls, iicActor, cmdKeys):
